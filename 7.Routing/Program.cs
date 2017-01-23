546293ff46d441de21e7f88809c5d7f87a6aa81e
CTRAN-DESKTOP\tranbacuong <ctran@amaris.com>
2017-01-23 14:25:39 +0700

    router key

---
+++ 7.Routing/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace _7.Routing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+                Console.WriteLine("enter your message");
+                var message = string.Empty;
+                while (message != "exit")
+                {
+                    message = Console.ReadLine();
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "direct_logs",
+                                     routingKey: "severity",
+                                     basicProperties: null,
+                                     body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
